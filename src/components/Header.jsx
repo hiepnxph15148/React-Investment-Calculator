33d1a90a6c5c45b3cf18c496725c8e6fb7338be1
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-16 16:04:52 +0700

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import imageHeader from '../assets/investment-calculator-logo.png'
+
+const Header = () => {
+  return (
+    <header>
+        <img src={imageHeader} alt="" />
+        <h2></h2>
+    </header>
+  )
+}
+
+export default Header
