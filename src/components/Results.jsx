bd2c7e98cf8bd415dfeaa63c71c0ffb8ae68a4e6
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-19 18:05:42 +0700

    fomart resultsData

---
+++ src/components/Results.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {calculateInvestmentResults} from '../util/investment.js'
+
+const Results = ({input}) => {
+  const resultsData = calculateInvestmentResults(input)
+  console.log(resultsData)
+  return (
+    <p>Results</p>
+  )
+}
+
+export default Results 
