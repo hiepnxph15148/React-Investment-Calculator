121bf80331e3aed7f5edf0b949b1b94fa012efd7
hiepnxph15148 <hiepnxph15148@fpt.edu.vn>
2024-08-20 14:44:03 +0700

    Mục 5 : React Essentials - Practice Project Thêm cmt code cho từng line khi đọc lại

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,18 @@ import UserInput from "./components/UserInput"
 import { useState } from 'react'
 
 function App() {
+  // Khai báo state cho userInput
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   })
+
+  // Khởi tạo ban đầu bắt buộc phải lớn hơn 0
+  const isInputValue = userInput.duration >= 1;
+
+  // Xử lý sự kiện thay đ��i giá trị input
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
       return {
@@ -22,7 +28,8 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!isInputValue && <p className="centerd">Please enter valid values for duration and investment duration.</p>}
+      {isInputValue && <Results input={userInput} />}
     </>
   )
 }
